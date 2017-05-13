765b4ddca4698dd1327bc3ec7cdcc87bfcce06f8
Dustin Decker <sevoma@gmail.com>
2017-05-13 18:06:34 -0500

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# deflEK
+
+Reverse proxy to add index-level RBAC to Elasticsearch or Kibana
+
+This is an early POC. It is not close to being feature complete. Use at your own risk.
+
+Features:
+- whitelist indicies that can be queried in Kibana
---
+++ deflek.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/sevoma/goutil"
+	"github.com/spf13/viper"
+)
+
+type SearchReq struct {
+	Index []string               `json:"index"`
+	X     map[string]interface{} `json:"-"` // Rest of the fields should go here.
+}
+
+type Prox struct {
+	target        *url.URL
+	proxy         *httputil.ReverseProxy
+	routePatterns []*regexp.Regexp
+}
+
+func New(target string) *Prox {
+	url, _ := url.Parse(target)
+
+	return &Prox{target: url, proxy: httputil.NewSingleHostReverseProxy(url)}
+}
+
+func (p *Prox) handle(w http.ResponseWriter, r *http.Request) {
+
+	if p.routePatterns == nil || p.parseWhiteList(r) {
+		p.proxy.ServeHTTP(w, r)
+	}
+}
+
+func (p *Prox) parseWhiteList(r *http.Request) bool {
+	search, _ := regexp.Compile(`^\/elasticsearch\/_msearch`)
+	whitelistIndex := []string{".kibana", "metrics"}
+	if search.MatchString(r.URL.Path) {
+		buf, _ := ioutil.ReadAll(r.Body)
+		rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
+		rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf))
+		r.Body = rdr2
+		decoder := json.NewDecoder(rdr1)
+		f := SearchReq{}
+		err := decoder.Decode(&f)
+		if err != nil {
+			return false
+		}
+		// add the rest of the fields
+		err = decoder.Decode(&f.X)
+		if err != nil {
+			return false
+		}
+		for _, val := range f.Index {
+			if !goutil.StringInSlice(val, whitelistIndex) {
+				fmt.Printf("%s not in index whitelist", val)
+				return false
+			}
+		}
+	}
+	for _, regexp := range p.routePatterns {
+		fmt.Println(r.URL.Path)
+		path := strings.TrimPrefix(r.URL.Path, viper.GetString("targetPathPrefix"))
+		if !regexp.MatchString(path) {
+			fmt.Printf("Not accepted routes %x", r.URL.Path)
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	// Get config
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() // yaml, toml, json, ini, whatever
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s", err))
+	}
+
+	port := viper.GetString("port")
+	target := viper.GetString("target")
+	whitelistedRoutes := viper.GetString("whitelistedRoutes")
+
+	reg, err := regexp.Compile(whitelistedRoutes)
+	if err != nil {
+		panic(fmt.Errorf("Error compiling whitelistedRoutes regex: %s", err))
+	}
+	routes := []*regexp.Regexp{reg}
+
+	proxy := New(target)
+	proxy.routePatterns = routes
+
+	http.HandleFunc("/", proxy.handle)
+	http.ListenAndServe(":"+port, nil)
+}
