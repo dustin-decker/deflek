42efa2dec3612f96618889c8ba2f8076678b2054
Dustin Decker <sevoma@gmail.com>
2017-05-14 00:26:14 -0500

    added config based RBAC

--- deflek.go
+++ deflek.go
@@ -21,32 +21,79 @@ type SearchReq struct {
 }
 
 type Prox struct {
+	config        *Config
 	target        *url.URL
 	proxy         *httputil.ReverseProxy
 	routePatterns []*regexp.Regexp
 }
 
-func New(target string) *Prox {
-	url, _ := url.Parse(target)
+type Config struct {
+	Port              string
+	Target            string
+	TargetPathPrefix  string
+	WhitelistedRoutes string
+	RBAC              struct {
+		Groups map[string]Permissions
+		Users  map[string]Permissions
+	}
+}
 
-	return &Prox{target: url, proxy: httputil.NewSingleHostReverseProxy(url)}
+type Permissions struct {
+	WhitelistedIndices []string
+	CanManage          bool
+}
+
+func NewProx(C *Config) *Prox {
+	url, _ := url.Parse(C.Target)
+
+	return &Prox{config: C, target: url, proxy: httputil.NewSingleHostReverseProxy(url)}
 }
 
 func (p *Prox) handle(w http.ResponseWriter, r *http.Request) {
 
-	if p.checkWhiteLists(r) {
+	if p.checkWhiteLists(r, p.config) {
 		p.proxy.ServeHTTP(w, r)
 	}
 }
 
-func (p *Prox) checkWhiteLists(r *http.Request) bool {
+func (p *Prox) checkWhiteLists(r *http.Request, C *Config) bool {
+
+	// Username is trusted input provided by a SSO proxy layer
+	var username string
+	if _, ok := r.Header["Username"]; ok {
+		username = r.Header["Username"][0]
+	} else {
+		return false
+	}
+
+	var groups []string
+	// Group is trusted input provided by a SSO proxy layer
+	if _, ok := r.Header["Groups"]; ok {
+		groups = r.Header["Groups"]
+	}
+	var whitelistedIndices []string
+	for _, group := range groups {
+		if _, ok := C.RBAC.Groups[group]; ok {
+			whitelistedIndices = append(whitelistedIndices,
+				C.RBAC.Groups[group].WhitelistedIndices...)
+		}
+	}
+	if _, ok := C.RBAC.Users[username]; ok {
+		for _, group := range C.RBAC.Users[username].WhitelistedIndices {
+			if !goutil.StringInSlice(group, whitelistedIndices) {
+				whitelistedIndices = append(whitelistedIndices, group)
+			}
+		}
+	}
+	fmt.Printf("User %s permitted to %s indices", username, whitelistedIndices)
 
 	// Check Kibana queries against whitelisted indices
 	search, _ := regexp.Compile(`^\/elasticsearch\/_msearch`)
-	whitelistedIndicies := viper.GetStringSlice("whitelistedIndices")
 	if search.MatchString(r.URL.Path) {
 		buf, _ := ioutil.ReadAll(r.Body)
 		rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
+		// If we don't keep a second reader untouched, we will consume
+		// the request body when reading it
 		rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf))
 		r.Body = rdr2
 		decoder := json.NewDecoder(rdr1)
@@ -61,7 +108,7 @@ func (p *Prox) checkWhiteLists(r *http.Request) bool {
 			return false
 		}
 		for _, val := range f.Index {
-			if !goutil.StringInSlice(val, whitelistedIndicies) {
+			if !goutil.StringInSlice(val, whitelistedIndices) {
 				fmt.Printf("%s not in index whitelist", val)
 				return false
 			}
@@ -71,7 +118,7 @@ func (p *Prox) checkWhiteLists(r *http.Request) bool {
 	// Check against whitelisted routes
 	for _, regexp := range p.routePatterns {
 		fmt.Println(r.URL.Path)
-		path := strings.TrimPrefix(r.URL.Path, viper.GetString("targetPathPrefix"))
+		path := strings.TrimPrefix(r.URL.Path, viper.GetString("TargetPathPrefix"))
 		if !regexp.MatchString(path) {
 			fmt.Printf("Not accepted routes %x", r.URL.Path)
 			return false
@@ -89,9 +136,13 @@ func main() {
 		panic(fmt.Errorf("Fatal error config file: %s", err))
 	}
 
-	port := viper.GetString("port")
-	target := viper.GetString("target")
-	whitelistedRoutes := viper.GetString("whitelistedRoutes")
+	var C Config
+	err = viper.Unmarshal(&C)
+	if err != nil {
+		panic(fmt.Errorf("unable to decode into struct, %v", err))
+	}
+
+	whitelistedRoutes := viper.GetString("WhitelistedRoutes")
 
 	reg, err := regexp.Compile(whitelistedRoutes)
 	if err != nil {
@@ -99,9 +150,9 @@ func main() {
 	}
 	routes := []*regexp.Regexp{reg}
 
-	proxy := New(target)
+	proxy := NewProx(&C)
 	proxy.routePatterns = routes
 
 	http.HandleFunc("/", proxy.handle)
-	http.ListenAndServe(":"+port, nil)
+	http.ListenAndServe(fmt.Sprintf(":%s", C.Port), nil)
 }
