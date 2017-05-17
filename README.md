fc51e3398275db85b3d7a7baffb042c6d3074877
Dustin Decker <sevoma@gmail.com>
2017-05-17 15:37:33 -0500

    refactored access control logic. it is simpler and more efficient now. squashed a yaml bug

--- README.md
+++ README.md
@@ -4,11 +4,10 @@ Reverse proxy to add index-level RBAC to Elasticsearch (soon) or Kibana
 
 This is an early POC. It is not close to being feature complete. Use at your own risk.
 
-It currently requires fronting with a SSO authentication proxy to pass Username and Group headers for RBAC lookup. deflEK assumes this is trusted input. If that is not true for your use case, you MUST add authentication middleware.
+It currently requires fronting with a SSO authentication proxy to pass Username and Group headers for RBAC lookup. deflEK assumes these headers are trusted input. If that is not true for your use case, you MUST add authentication middleware.
 
 Features:
 - RBAC on indicies that can be queries in Kibana
-- RBAC on Kibana settings (index patterns, cached field lists, dev console...)
 
 Desired Features:
 - Kibana feature toggles (index pattern management, console, etc)
--- deflek.go
+++ deflek.go
@@ -6,14 +6,14 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"regexp"
 	"strings"
 
-	"github.com/sevoma/goutil"
-	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
 )
 
 // SearchReq unmarshalls index field from Kibana json request
@@ -33,12 +33,12 @@ type Prox struct {
 // Config for reverse proxy settings and RBAC users and groups
 // Unmarshalled from config on disk
 type Config struct {
-	ListenInterface      string
-	ListenPort           int
+	ListenInterface      string `yaml:"listen_interface"`
+	ListenPort           int    `yaml:"listen_port"`
 	Target               string
-	TargetPathPrefix     string
-	WhitelistedRoutes    string
-	AnonymousMetricsUser bool
+	TargetPathPrefix     string `yaml:"target_path_prefix"`
+	WhitelistedRoutes    string `yaml:"whitelisted_routes"`
+	AnonymousMetricsUser bool   `yaml:"anonymous_metrics_user"`
 	RBAC                 struct {
 		Groups map[string]Permissions
 		Users  map[string]Permissions
@@ -47,8 +47,13 @@ type Config struct {
 
 // Permissions structure for groups and users
 type Permissions struct {
-	WhitelistedIndices []string
-	CanManage          bool
+	WhitelistedIndices map[string]Index `yaml:"whitelisted_indices"`
+	CanManage          bool             `yaml:"can_manage"`
+}
+
+// Index struct defines index and REST verbs allowed
+type Index struct {
+	RESTverbs []string
 }
 
 // NewProx returns new reverse proxy instance
@@ -67,7 +72,6 @@ func (p *Prox) handle(w http.ResponseWriter, r *http.Request) {
 
 func (p *Prox) checkWhiteLists(r *http.Request, C *Config) bool {
 	path := strings.TrimPrefix(r.URL.Path, C.TargetPathPrefix)
-	permissions, err := GetPermissions(r, C)
 
 	// Check against whitelisted routes
 	for _, regexp := range p.routePatterns {
@@ -78,24 +82,19 @@ func (p *Prox) checkWhiteLists(r *http.Request, C *Config) bool {
 		}
 	}
 
-	// Control index pattern access
-	if !permissions.CanManage && strings.HasPrefix(path, "/elasticsearch/.kibana/index-pattern") && !strings.HasPrefix(path, "/elasticsearch/.kibana/index-pattern/_search") {
-		fmt.Printf("Cannot manage %s", r.URL.Path)
-		return false
-	}
-	// Control api console access
-	if !permissions.CanManage && strings.HasPrefix(path, "/api/console") {
-		fmt.Printf("Cannot manage %s", r.URL.Path)
-		return false
-	}
+	// // Control index pattern access
+	// if !permissions.CanManage && strings.HasPrefix(path, "/elasticsearch/.kibana/index-pattern") && !strings.HasPrefix(path, "/elasticsearch/.kibana/index-pattern/_search") {
+	// 	fmt.Printf("Cannot manage %s", r.URL.Path)
+	// 	return false
+	// }
+	// // Control api console access
+	// if !permissions.CanManage && strings.HasPrefix(path, "/api/console") {
+	// 	fmt.Printf("Cannot manage %s", r.URL.Path)
+	// 	return false
+	// }
 
 	// Check Kibana queries against whitelisted indices
 	if strings.HasPrefix(path, "/elasticsearch/_msearch") {
-
-		if err != nil {
-			fmt.Println(err.Error())
-			return false
-		}
 
 		buf, _ := ioutil.ReadAll(r.Body)
 		rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
@@ -105,7 +104,7 @@ func (p *Prox) checkWhiteLists(r *http.Request, C *Config) bool {
 		r.Body = rdr2
 		decoder := json.NewDecoder(rdr1)
 		f := SearchReq{}
-		err = decoder.Decode(&f)
+		err := decoder.Decode(&f)
 		if err != nil {
 			return false
 		}
@@ -114,10 +113,14 @@ func (p *Prox) checkWhiteLists(r *http.Request, C *Config) bool {
 		if err != nil {
 			return false
 		}
+		// Check if index is is the whitelist
 		for _, index := range f.Index {
-			suffix := regexp.MustCompile(`([a-zA-z0-9]*)-[0-9]{4}\.[0-9]{2}\.[0-9]{2}`)
-			trimmedIndex := suffix.ReplaceAllString(index, "${1}")
-			if !goutil.StringInSlice(trimmedIndex, permissions.WhitelistedIndices) {
+			permitted, err := indexPermitted(index, r, C)
+			if err != nil {
+				fmt.Println(err.Error())
+				return false
+			}
+			if !permitted {
 				fmt.Printf("%s not in index whitelist", index)
 				return false
 			}
@@ -127,71 +130,75 @@ func (p *Prox) checkWhiteLists(r *http.Request, C *Config) bool {
 	return true
 }
 
-// GetPermissions returns indices whitelisted for the user and group provided
-func GetPermissions(r *http.Request, C *Config) (Permissions, error) {
-
-	var whitelistedIndices []string
-
+func getUser(r *http.Request, C *Config) (string, error) {
 	// Username is trusted input provided by a SSO proxy layer
 	var username string
-	var userCanManage bool
 	if _, ok := r.Header["Username"]; ok {
 		username = r.Header["Username"][0]
 	} else {
 		if C.AnonymousMetricsUser {
 			username = "metrics"
 		} else {
-			return Permissions{}, errors.New(("No Username header provided"))
+			return "", errors.New(("No Username header provided"))
 		}
 	}
+	return username, nil
+}
 
+func getGroups(r *http.Request, C *Config) ([]string, error) {
 	// Group is trusted input provided by a SSO proxy layer
 	var groups []string
 	if _, ok := r.Header["Groups"]; ok {
 		groups = r.Header["Groups"]
 	}
 
-	groupCanManage := false
+	return groups, nil
+}
+
+func indexPermitted(index string, r *http.Request, C *Config) (bool, error) {
+	suffix := regexp.MustCompile(`([.a-zA-z0-9]*)-[0-9]{4}\.[0-9]{2}\.[0-9]{2}`)
+	index = suffix.ReplaceAllString(index, "${1}")
+
+	username, err := getUser(r, C)
+	if err != nil {
+		return false, errors.New("No username provided")
+	}
+	groups, _ := getGroups(r, C)
+
 	for _, group := range groups {
-		if _, ok := C.RBAC.Groups[group]; ok {
-			if C.RBAC.Groups[group].CanManage == true {
-				groupCanManage = true
-			}
-			whitelistedIndices = append(whitelistedIndices,
-				C.RBAC.Groups[group].WhitelistedIndices...)
-		}
-	}
-	if _, ok := C.RBAC.Users[username]; ok {
-		for _, group := range C.RBAC.Users[username].WhitelistedIndices {
-			if !goutil.StringInSlice(group, whitelistedIndices) {
-				whitelistedIndices = append(whitelistedIndices, group)
+		if configGroup, ok := C.RBAC.Groups[group]; ok {
+			if _, ok := configGroup.WhitelistedIndices[index]; ok {
+				return true, nil
 			}
 		}
 	}
 
-	userCanManage = C.RBAC.Users[username].CanManage
+	if configUser, ok := C.RBAC.Users[username]; ok {
+		if _, ok := configUser.WhitelistedIndices[index]; ok {
+			return true, nil
+		}
+	}
 
-	permissions := Permissions{WhitelistedIndices: whitelistedIndices,
-		CanManage: (userCanManage || groupCanManage)}
+	return false, nil
+}
 
-	return permissions, nil
+func (C *Config) getConf() *Config {
+
+	yamlFile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, C)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	return C
 }
 
 func main() {
-	// Get config
-	viper.SetConfigName("config") // name of config file (without extension)\
-	// yaml, toml, json, ini, it don't care
-	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s", err))
-	}
-
 	var C Config
-	err = viper.Unmarshal(&C)
-	if err != nil {
-		panic(fmt.Errorf("Unable to decode config into struct: %v", err))
-	}
+	C.getConf()
 
 	reg, err := regexp.Compile(C.WhitelistedRoutes)
 	if err != nil {
