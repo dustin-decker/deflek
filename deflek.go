510848027e3b44904140d1ca0960e3806eea47dd
Dustin Decker <sevoma@gmail.com>
2017-11-30 04:36:29 -0600

    more refactor

--- deflek.go
+++ deflek.go
@@ -334,6 +334,42 @@ func filterMsearch(ctx requestContext) (bool, error) {
 	return true, err
 }
 
+func filterNamedIndex(ctx requestContext) (bool, error) {
+	// Index API endpoints begin with the index name
+	// Other API endpoints begin with '_'
+
+	ctx.trace.Index = []string{ctx.index}
+	if ctx.r.Method == "POST" {
+		body, err := getBody(ctx.r)
+		if err != nil {
+			return false, err
+		}
+		ctx.trace.Query = string(body)
+	}
+
+	// req'd by Visual Builder
+	if ctx.r.URL.Path == "/*/_field_stats" {
+		return true, nil
+	}
+
+	// TODO for Visual Builder
+	// Replace * with all indices they have access to... :(
+	//  {"code":403,"elasped":0,"groups":"[group2]","index":"[*]","lvl":"eror","method":"POST","msg":"* not in index whitelist","path":"/_msearch","query":"{\"index\":[\"*\"],\"ignore\":[404],\"timeout\":\"90s\",\"requestTimeout\":90000,\"ignoreUnavailable\":true}\n{\"size\":0,\"query\":{\"bool\":{\"must\":[{\"range\":{\"@timestamp\":{\"gte\":1339990677678,\"lte\":1497152277679,\"format\":\"epoch_millis\"}}},{\"bool\":{\"must\":[{\"query_string\":{\"query\":\"*\"}}],\"must_not\":[]}}]}},\"aggs\":{\"ec3c3e41-53d5-11e7-80ea-7bfec2933998\":{\"filter\":{\"match_all\":{}},\"aggs\":{\"timeseries\":{\"date_histogram\":{\"field\":\"@timestamp\",\"interval\":\"604800s\",\"min_doc_count\":0,\"extended_bounds\":{\"min\":1339990677678,\"max\":1497152277679}},\"aggs\":{\"ec3c3e42-53d5-11e7-80ea-7bfec2933998\":{\"bucket_script\":{\"buckets_path\":{\"count\":\"_count\"},\"script\":{\"inline\":\"count * 1\",\"lang\":\"expression\"},\"gap_policy\":\"skip\"}}}}}}}}\n","t":"2017-06-18T03:37:57.786407134Z","user":""}
+
+	for _, whitelistedIndex := range ctx.whitelistedIndices {
+		if glob.Glob(whitelistedIndex.Name, ctx.index) {
+			for _, method := range whitelistedIndex.RESTverbs {
+				if ctx.r.Method == method {
+					return true, nil
+				}
+			}
+			return false, errors.New("Method not allowed on index")
+		}
+	}
+	return false, errors.New("Index not allowed")
+
+}
+
 func indexPermitted(trace *AppTrace, r *http.Request, C *Config) (bool, error) {
 	index := strings.Split(r.URL.Path, "/")[1]
 
@@ -363,42 +399,9 @@ func indexPermitted(trace *AppTrace, r *http.Request, C *Config) (bool, error) {
 		trace.Query = string(body)
 		return false, errors.New("Searching with is not supported at this time")
 
+	case !strings.HasPrefix(index, "_") && len(index) > 0:
+
 	}
-
-	// Index API endpoints begin with the index name
-	// Other API endpoints begin with '_'
-	if !strings.HasPrefix(index, "_") && len(index) > 0 {
-		trace.Index = []string{index}
-		if r.Method == "POST" {
-			body, err := getBody(r)
-			if err != nil {
-				return false, err
-			}
-			trace.Query = string(body)
-		}
-
-		// req'd by Visual Builder
-		if r.URL.Path == "/*/_field_stats" {
-			return true, nil
-		}
-
-		// TODO for Visual Builder
-		// Replace * with all indices they have access to... :(
-		//  {"code":403,"elasped":0,"groups":"[group2]","index":"[*]","lvl":"eror","method":"POST","msg":"* not in index whitelist","path":"/_msearch","query":"{\"index\":[\"*\"],\"ignore\":[404],\"timeout\":\"90s\",\"requestTimeout\":90000,\"ignoreUnavailable\":true}\n{\"size\":0,\"query\":{\"bool\":{\"must\":[{\"range\":{\"@timestamp\":{\"gte\":1339990677678,\"lte\":1497152277679,\"format\":\"epoch_millis\"}}},{\"bool\":{\"must\":[{\"query_string\":{\"query\":\"*\"}}],\"must_not\":[]}}]}},\"aggs\":{\"ec3c3e41-53d5-11e7-80ea-7bfec2933998\":{\"filter\":{\"match_all\":{}},\"aggs\":{\"timeseries\":{\"date_histogram\":{\"field\":\"@timestamp\",\"interval\":\"604800s\",\"min_doc_count\":0,\"extended_bounds\":{\"min\":1339990677678,\"max\":1497152277679}},\"aggs\":{\"ec3c3e42-53d5-11e7-80ea-7bfec2933998\":{\"bucket_script\":{\"buckets_path\":{\"count\":\"_count\"},\"script\":{\"inline\":\"count * 1\",\"lang\":\"expression\"},\"gap_policy\":\"skip\"}}}}}}}}\n","t":"2017-06-18T03:37:57.786407134Z","user":""}
-
-		for _, whitelistedIndex := range whitelistedIndices {
-			if glob.Glob(whitelistedIndex.Name, index) {
-				for _, method := range whitelistedIndex.RESTverbs {
-					if r.Method == method {
-						return true, nil
-					}
-				}
-				return false, errors.New("Method not allowed on index")
-			}
-		}
-		return false, errors.New("Index not allowed")
-	}
-
 	return true, nil
 }
 
