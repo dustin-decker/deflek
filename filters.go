783a0e59dfbee8e4d4e85a5a3a2c3c38cbb226d1
Dustin Decker <dustindecker@protonmail.com>
2018-02-21 04:50:16 -0600

    generic extractors

--- filters.go
+++ filters.go
@@ -1,99 +1,22 @@
 package main
 
-import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"reflect"
+// // req'd by Visual Builder
+// if ctx.r.URL.Path == "/*/_field_stats" {
+// 	return true, nil
+// }
 
-	glob "github.com/ryanuber/go-glob"
-)
+// TODO for Visual Builder
+// Replace * with all indices they have access to... :(
+//  {"code":403,"elasped":0,"groups":"[group2]","index":"[*]","lvl":"eror","method":"POST","msg":"* not in index whitelist","path":"/_msearch","query":"{\"index\":[\"*\"],\"ignore\":[404],\"timeout\":\"90s\",\"requestTimeout\":90000,\"ignoreUnavailable\":true}\n{\"size\":0,\"query\":{\"bool\":{\"must\":[{\"range\":{\"@timestamp\":{\"gte\":1339990677678,\"lte\":1497152277679,\"format\":\"epoch_millis\"}}},{\"bool\":{\"must\":[{\"query_string\":{\"query\":\"*\"}}],\"must_not\":[]}}]}},\"aggs\":{\"ec3c3e41-53d5-11e7-80ea-7bfec2933998\":{\"filter\":{\"match_all\":{}},\"aggs\":{\"timeseries\":{\"date_histogram\":{\"field\":\"@timestamp\",\"interval\":\"604800s\",\"min_doc_count\":0,\"extended_bounds\":{\"min\":1339990677678,\"max\":1497152277679}},\"aggs\":{\"ec3c3e42-53d5-11e7-80ea-7bfec2933998\":{\"bucket_script\":{\"buckets_path\":{\"count\":\"_count\"},\"script\":{\"inline\":\"count * 1\",\"lang\":\"expression\"},\"gap_policy\":\"skip\"}}}}}}}}\n","t":"2017-06-18T03:37:57.786407134Z","user":""}
 
-func filterMsearch(ctx requestContext) (bool, error) {
-	body, err := getBody(ctx.r)
-	if err != nil {
-		return false, err
-	}
-	ctx.trace.Query = string(body)
+// fmt.Println(ctx.whitelistedIndices)
+// for _, whitelistedIndex := range ctx.whitelistedIndices {
+// 	for _, index := range ctx.indices {
+// 		if glob.Glob(whitelistedIndex.Name, index) {
+// 			return true, nil
+// 		}
+// 	}
+// }
+// return false, nil
 
-	// They're sending newline deliminated JSON blobs :/
-	// Savages.
-	firstRoot := bytes.Split(body, []byte("\n"))[0]
-
-	f := SearchReq{}
-	err = json.Unmarshal(firstRoot, &f)
-	if err != nil {
-		return false, err
-	}
-	err = json.Unmarshal(firstRoot, &f.X)
-	if err != nil {
-		return false, err
-	}
-
-	// Trace indices
-	var indices []string
-	switch jv := f.Index.(type) {
-	// ES 2.X uses index string
-	case string:
-		indices = []string{jv}
-	// ES 5 uses JSON array of indices
-	case []interface{}:
-		for _, v := range jv {
-			indices = append(indices, v.(string))
-		}
-	default:
-		return false, fmt.Errorf("Unknown type %v returned for json field 'index'",
-			reflect.TypeOf(f.Index))
-	}
-	ctx.trace.Index = indices
-
-	// Check if index is is the whitelist
-	for _, index := range indices {
-		permitted, err := indexBodyPermitted(index, ctx.r, ctx.c)
-		if err != nil {
-			return false, err
-		}
-		if !permitted {
-			err = fmt.Errorf("%s not in index whitelist", index)
-			return false, err
-		}
-	}
-	return true, err
-}
-
-func filterNamedIndex(ctx requestContext) (bool, error) {
-	// Index API endpoints begin with the index name
-	// Other API endpoints begin with '_'
-
-	ctx.trace.Index = []string{ctx.index}
-	if ctx.r.Method == "POST" {
-		body, err := getBody(ctx.r)
-		if err != nil {
-			return false, err
-		}
-		ctx.trace.Query = string(body)
-	}
-
-	// req'd by Visual Builder
-	if ctx.r.URL.Path == "/*/_field_stats" {
-		return true, nil
-	}
-
-	// TODO for Visual Builder
-	// Replace * with all indices they have access to... :(
-	//  {"code":403,"elasped":0,"groups":"[group2]","index":"[*]","lvl":"eror","method":"POST","msg":"* not in index whitelist","path":"/_msearch","query":"{\"index\":[\"*\"],\"ignore\":[404],\"timeout\":\"90s\",\"requestTimeout\":90000,\"ignoreUnavailable\":true}\n{\"size\":0,\"query\":{\"bool\":{\"must\":[{\"range\":{\"@timestamp\":{\"gte\":1339990677678,\"lte\":1497152277679,\"format\":\"epoch_millis\"}}},{\"bool\":{\"must\":[{\"query_string\":{\"query\":\"*\"}}],\"must_not\":[]}}]}},\"aggs\":{\"ec3c3e41-53d5-11e7-80ea-7bfec2933998\":{\"filter\":{\"match_all\":{}},\"aggs\":{\"timeseries\":{\"date_histogram\":{\"field\":\"@timestamp\",\"interval\":\"604800s\",\"min_doc_count\":0,\"extended_bounds\":{\"min\":1339990677678,\"max\":1497152277679}},\"aggs\":{\"ec3c3e42-53d5-11e7-80ea-7bfec2933998\":{\"bucket_script\":{\"buckets_path\":{\"count\":\"_count\"},\"script\":{\"inline\":\"count * 1\",\"lang\":\"expression\"},\"gap_policy\":\"skip\"}}}}}}}}\n","t":"2017-06-18T03:37:57.786407134Z","user":""}
-
-	for _, whitelistedIndex := range ctx.whitelistedIndices {
-		if glob.Glob(whitelistedIndex.Name, ctx.index) {
-			for _, method := range whitelistedIndex.RESTverbs {
-				if ctx.r.Method == method {
-					return true, nil
-				}
-			}
-			return false, errors.New("Method not allowed on index")
-		}
-	}
-	return false, errors.New("Index not allowed")
-
-}
+// }
---
+++ integration_test.go
@@ -0,0 +1,255 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/olivere/elastic"
+)
+
+type authTransport struct {
+	Transport http.RoundTripper
+}
+
+func (t *authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Add("X-Remote-User", "dustind")
+	req.Header.Add("X-Remote-Groups", "OU=thing,CN=group2,DC=something")
+	tr := &http.Transport{}
+	res, err := tr.RoundTrip(req)
+	return res, err
+}
+
+func createEsClient() *elastic.Client {
+	ctx := context.Background()
+	url := "http://127.0.0.1:8080"
+	sniff := true
+
+	httpClient := &http.Client{
+		Transport: &authTransport{},
+		Timeout:   10 * time.Second,
+	}
+
+	c, err := elastic.NewClient(elastic.SetURL(url),
+		elastic.SetSniff(sniff),
+		elastic.SetHttpClient(httpClient))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testIndices := []string{"test_deflek", "secret_stuff"}
+	for _, index := range testIndices {
+		exists, err := c.IndexExists(index).Do(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if !exists {
+			c.CreateIndex(index).Do(ctx)
+			c.Index().Index("secret_stuff").Id("1").OpType("index")
+			c.Index().Index("test_deflek").Id("1").OpType("index")
+		}
+	}
+	return c
+}
+
+func createHTTPClient() *http.Client {
+	httpClient := &http.Client{
+		Transport: &authTransport{},
+		Timeout:   5 * time.Second,
+	}
+	return httpClient
+}
+
+func testAllowed(t *testing.T, res *http.Response) {
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error("couldn't read the body. got: ", err)
+	}
+	if res.StatusCode != 200 {
+		t.Errorf("request should have been allowed. got: \n status code: %v \nbody: %s", res.StatusCode, string(body))
+	}
+}
+
+func testBlocked(t *testing.T, res *http.Response) {
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error("couldn't read the body. got: ", err)
+	}
+	if res.StatusCode != 401 {
+		t.Errorf("request should have been blocked. got: \n status code: %v \nbody: %s", res.StatusCode, string(body))
+	}
+}
+
+const base = "http://127.0.0.1:8080"
+
+// func TestAll(t *testing.T) {
+// 	createEsClient()
+// 	httpC := createHTTPClient()
+
+// 	res, err := httpC.Get(base + "/_all/tweet/_search?q=tag:wow")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	testBlocked(t, res)
+// }
+
+// func TestSearch(t *testing.T) {
+// 	createEsClient()
+// 	httpC := createHTTPClient()
+
+// 	res, err := httpC.Get(base + "/_search?q=tag:wow")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	testBlocked(t, res)
+// }
+
+func TestMsearchBlocked(t *testing.T) {
+	createEsClient()
+	httpC := createHTTPClient()
+
+	body := `
+{"index" : "test_deflek"}
+{"query" : {"match_all" : {}}, "from" : 0, "size" : 10}
+{"index" : "secret_stuff", "search_type" : "dfs_query_then_fetch"}
+{"query" : {"match_all" : {}}}
+{}
+{"query" : {"match_all" : {}}}
+
+{"query" : {"match_all" : {}}}
+{"search_type" : "dfs_query_then_fetch"}
+{"query" : {"match_all" : {}}}
+`
+
+	res, err := httpC.Post(base+"/_msearch", "application/json", bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testBlocked(t, res)
+}
+
+func TestMsearchAllowed(t *testing.T) {
+	createEsClient()
+	httpC := createHTTPClient()
+
+	body := `
+{"index" : "test_deflek"}
+{"query" : {"match_all" : {}}, "from" : 0, "size" : 10}
+{"index" : "test_deflek", "search_type" : "dfs_query_then_fetch"}
+{"query" : {"match_all" : {}}}
+{}
+{"query" : {"match_all" : {}}}
+
+{"query" : {"match_all" : {}}}
+{"search_type" : "dfs_query_then_fetch"}
+{"query" : {"match_all" : {}}}
+`
+
+	res, err := httpC.Post(base+"/_msearch", "application/json", bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testAllowed(t, res)
+}
+
+func TestMgetBlocked(t *testing.T) {
+	createEsClient()
+	httpC := createHTTPClient()
+
+	body := `
+	{
+		"docs" : [
+			{
+				"_index" : "test_deflek",
+				"_id" : "1"
+			},
+			{
+				"_index" : "secret_stuff",
+				"_id" : "1"
+			}
+		]
+	}	
+	`
+
+	res, err := httpC.Post(base+"/_mget", "application/json", bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testBlocked(t, res)
+}
+
+func TestMgetAllowed(t *testing.T) {
+	createEsClient()
+	httpC := createHTTPClient()
+
+	body := `
+	{
+		"docs" : [
+			{
+				"_index" : "test_deflek",
+				"_id" : "1"
+			},
+			{
+				"_index" : "test_deflek",
+				"_id" : "2"
+			}
+		]
+	}	
+	`
+
+	res, err := httpC.Post(base+"/_mget", "application/json", bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testAllowed(t, res)
+}
+
+func TestNamedIndexBlock(t *testing.T) {
+	createEsClient()
+	httpC := createHTTPClient()
+
+	res, err := httpC.Get(base + "/secret_stuff/_search?q=tag:wow")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testBlocked(t, res)
+}
+
+func TestNamedIndexAllow(t *testing.T) {
+	createEsClient()
+	httpC := createHTTPClient()
+
+	res, err := httpC.Get(base + "/test_deflek/_search?q=tag:wow")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testAllowed(t, res)
+}
+
+//// This might not be a thing in 6.X!
+// func TestFieldStatsAllow(t *testing.T) {
+// 	createEsClient()
+// 	httpC := createHTTPClient()
+
+// 	res, err := httpC.Post(base+"/*/_field_stats",
+// 		"application/json",
+// 		bytes.NewReader([]byte("{}")))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	testAllowed(t, res)
+// }
