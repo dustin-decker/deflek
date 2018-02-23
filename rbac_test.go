a5f9bb0cf40cf81b41b5533928f949ebfbb984b3
Dustin Decker <dustindecker@protonmail.com>
2018-02-23 10:25:08 -0600

    test indexPermitted

--- rbac_test.go
+++ rbac_test.go
@@ -1,11 +1,51 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func getTestContext(path string) (*requestContext, error) {
+	req, _ := http.NewRequest("GET", "http://localhost:9200"+path, bytes.NewBufferString(""))
+	req.Header.Add("X-Remote-User", "dustind")
+	req.Header.Add("X-Remote-Groups", "OU=thing,CN=group2,DC=something")
+
+	var c Config
+	c.getConf()
+
+	var trace Trace
+
+	ctx, err := getRequestContext(req, &c, &trace)
+
+	return ctx, err
+}
+
+func TestIndexPermitted(t *testing.T) {
+	ctx, err := getTestContext("/*/_search")
+	if err != nil {
+		t.Error("could not get context: ", err)
+	}
+
+	ok, err := indexPermitted(ctx)
+	if !ok || err != nil {
+		t.Error("index not permitted or err: ", err)
+	}
+}
+
+func TestIndexNotPermitted(t *testing.T) {
+	ctx, err := getTestContext("/secret_stuff/_search")
+	if err != nil {
+		t.Error("could not get context: ", err)
+	}
+
+	ok, err := indexPermitted(ctx)
+	if ok || err != nil {
+		t.Error("index permitted or err: ", err)
+	}
+}
 
 func TestGetWhitelistedIndices(t *testing.T) {
 
