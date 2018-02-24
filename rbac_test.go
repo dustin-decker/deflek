045cab466e7e6d070e20dff6d245a494e8f56955
Dustin Decker <dustindecker@protonmail.com>
2018-02-23 19:47:05 -0600

    test checkRBAC

--- rbac_test.go
+++ rbac_test.go
@@ -127,6 +127,24 @@ func TestGetUser(t *testing.T) {
 	}
 }
 
+func TestCheckRBAC(t *testing.T) {
+	body := `{"index":"*","ignore":[404],"timeout":"90s","requestTimeout":90000,"ignoreUnavailable":true}
+{"size":0,"query":{"bool":{"must":[{"range":{"@timestamp":{"gte":1519223869113,"lte":1519225669114,"format":"epoch_millis"}}},{"bool":{"must":[{"match_all":{}}],"must_not":[]}}]}},"aggs":{"61ca57f1-469d-11e7-af02-69e470af7417":{"filter":{"match_all":{}},"aggs":{"timeseries":{"date_histogram":{"field":"@timestamp","interval":"30s","min_doc_count":0,"time_zone":"America/Chicago","extended_bounds":{"min":1519223869113,"max":1519225669114}},"aggs":{"61ca57f2-469d-11e7-af02-69e470af7417":{"bucket_script":{"buckets_path":{"count":"_count"},"script":{"inline":"count * 1","lang":"expression"},"gap_policy":"skip"}}}}}}}}
+`
+
+	ctx, err := getTestContext("/*/_search", body)
+	if err != nil {
+		t.Error("could not get context: ", err)
+	}
+
+	var p Prox
+
+	ok, err := p.checkRBAC(ctx)
+	if !ok || err != nil {
+		t.Error("index not permitted or err: ", err)
+	}
+}
+
 func indexInSlice(a Index, indices []Index) bool {
 	for _, b := range indices {
 		if b.Name == a.Name {
