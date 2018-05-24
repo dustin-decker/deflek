3974ee696cb1118bf368147a3278648f6c187dea
Dustin Decker <dustind@indeed.com>
2018-05-24 11:40:38 -0500

    remove debug message

--- rbac.go
+++ rbac.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -205,8 +204,6 @@ func apiPermitted(ctx *requestContext) (bool, error) {
 
 	if len(api) > 0 {
 		for _, whitelistedAPI := range ctx.whitelistedAPIs {
-			wAPI := whitelistedAPI
-			fmt.Println(wAPI)
 			// match API patterns in the RBAC config against patterns
 			// that were extracted (both support globs)
 			if glob.Glob(whitelistedAPI.Name, api) {
